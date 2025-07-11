614e3ad4882ea4ed00ab12251dd09c4794924bad
MihaiStan12 <casata552@gmail.com>
2025-07-12 01:00:29 +0300

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = document.getElementById('root');
+
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <h1>Bine ai venit la aplicația de timp de așteptare!</h1>
+    </React.StrictMode>
+  );
+}
